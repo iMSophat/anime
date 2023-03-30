a6119d568dc4ac840082246bcd7cd21e006b7f79
pphatDev <leatsophat02@gmail.com>
2023-03-30 18:51:48 +0700

    Update component

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,206 @@ __webpack_require__.r(__webpack_exports__);
   \**************************************/
 /***/ (() => {
 
+var playPauseBtn = document.querySelector(".play-pause-btn");
+var theaterBtn = document.querySelector(".theater-btn");
+var fullScreenBtn = document.querySelector(".full-screen-btn");
+var miniPlayerBtn = document.querySelector(".mini-player-btn");
+var muteBtn = document.querySelector(".mute-btn");
+var captionsBtn = document.querySelector(".captions-btn");
+var speedBtn = document.querySelector(".speed-btn");
+var currentTimeElem = document.querySelector(".current-time");
+var totalTimeElem = document.querySelector(".total-time");
+var previewImg = document.querySelector(".preview-img");
+var thumbnailImg = document.querySelector(".thumbnail-img");
+var volumeSlider = document.querySelector(".volume-slider");
+var videoContainer = document.querySelector(".video-container");
+var timelineContainer = document.querySelector(".timeline-container");
+var video = document.getElementById("video");
+document.addEventListener("keydown", function (e) {
+  var tagName = document.activeElement.tagName.toLowerCase();
+  if (tagName === "input") return;
+  switch (e.key.toLowerCase()) {
+    case " ":
+      if (tagName === "button") return;
+    case "k":
+      togglePlay();
+      break;
+    case "f":
+      toggleFullScreenMode();
+      break;
+    case "t":
+      toggleTheaterMode();
+      break;
+    case "i":
+      toggleMiniPlayerMode();
+      break;
+    case "m":
+      toggleMute();
+      break;
+    case "arrowleft":
+    case "j":
+      skip(-5);
+      break;
+    case "arrowright":
+    case "l":
+      skip(5);
+      break;
+    case "c":
+      toggleCaptions();
+      break;
+  }
+});
 
+// Timeline
+timelineContainer.addEventListener("mousemove", handleTimelineUpdate);
+timelineContainer.addEventListener("mousedown", toggleScrubbing);
+document.addEventListener("mouseup", function (e) {
+  if (isScrubbing) toggleScrubbing(e);
+});
+document.addEventListener("mousemove", function (e) {
+  if (isScrubbing) handleTimelineUpdate(e);
+});
+var isScrubbing = false;
+var wasPaused;
+function toggleScrubbing(e) {
+  var rect = timelineContainer.getBoundingClientRect();
+  var percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  isScrubbing = (e.buttons & 1) === 1;
+  videoContainer.classList.toggle("scrubbing", isScrubbing);
+  if (isScrubbing) {
+    wasPaused = video.paused;
+    video.pause();
+  } else {
+    video.currentTime = percent * video.duration;
+    if (!wasPaused) video.play();
+  }
+  handleTimelineUpdate(e);
+}
+function handleTimelineUpdate(e) {
+  var rect = timelineContainer.getBoundingClientRect();
+  var percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  var previewImgNumber = Math.max(1, Math.floor(percent * video.duration / 10));
+  var previewImgSrc = "".concat(window.origin, "/assets/preview/preview-").concat(previewImgNumber, ".jpg");
+  previewImg.src = previewImgSrc;
+  timelineContainer.style.setProperty("--preview-position", percent);
+  if (isScrubbing) {
+    e.preventDefault();
+    thumbnailImg.src = previewImgSrc;
+    timelineContainer.style.setProperty("--progress-position", percent);
+  }
+}
+
+// Playback Speed
+speedBtn.addEventListener("click", changePlaybackSpeed);
+function changePlaybackSpeed() {
+  var newPlaybackRate = video.playbackRate + 0.25;
+  if (newPlaybackRate > 2) newPlaybackRate = 0.25;
+  video.playbackRate = newPlaybackRate;
+  speedBtn.textContent = "".concat(newPlaybackRate, "x");
+}
+
+// Captions
+var captions = video.textTracks[0];
+captions.mode = "hidden";
+captionsBtn.addEventListener("click", toggleCaptions);
+function toggleCaptions() {
+  var isHidden = captions.mode === "hidden";
+  captions.mode = isHidden ? "showing" : "hidden";
+  videoContainer.classList.toggle("captions", isHidden);
+}
+
+// Duration
+video.addEventListener("loadeddata", function () {
+  totalTimeElem.textContent = formatDuration(video.duration);
+});
+video.addEventListener("timeupdate", function () {
+  currentTimeElem.textContent = formatDuration(video.currentTime);
+  var percent = video.currentTime / video.duration;
+  timelineContainer.style.setProperty("--progress-position", percent);
+});
+var leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2
+});
+function formatDuration(time) {
+  var seconds = Math.floor(time % 60);
+  var minutes = Math.floor(time / 60) % 60;
+  var hours = Math.floor(time / 3600);
+  if (hours === 0) {
+    return "".concat(minutes, ":").concat(leadingZeroFormatter.format(seconds));
+  } else {
+    return "".concat(hours, ":").concat(leadingZeroFormatter.format(minutes), ":").concat(leadingZeroFormatter.format(seconds));
+  }
+}
+function skip(duration) {
+  video.currentTime += duration;
+}
+
+// Volume
+muteBtn.addEventListener("click", toggleMute);
+volumeSlider.addEventListener("input", function (e) {
+  video.volume = e.target.value;
+  video.muted = e.target.value === 0;
+});
+function toggleMute() {
+  video.muted = !video.muted;
+}
+video.addEventListener("volumechange", function () {
+  volumeSlider.value = video.volume;
+  var volumeLevel;
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0;
+    volumeLevel = "muted";
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high";
+  } else {
+    volumeLevel = "low";
+  }
+  videoContainer.dataset.volumeLevel = volumeLevel;
+});
+
+// View Modes
+// theaterBtn.addEventListener("click", toggleTheaterMode)
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+function toggleFullScreenMode() {
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+function toggleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+document.addEventListener("fullscreenchange", function () {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+video.addEventListener("enterpictureinpicture", function () {
+  videoContainer.classList.add("mini-player");
+});
+video.addEventListener("leavepictureinpicture", function () {
+  videoContainer.classList.remove("mini-player");
+});
+
+// Play/Pause
+playPauseBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+video.addEventListener("play", function () {
+  videoContainer.classList.remove("paused");
+});
+video.addEventListener("pause", function () {
+  videoContainer.classList.add("paused");
+});
 
 /***/ }),
 
---
+++ resources/views/components/watching/profile.blade.php
@@ -0,0 +1,10 @@
+<div class="flex items-center relative w-1/2 p-3 border rounded-md">
+    <div class="w-16 flex-shrink-0">
+        <img class="w-16 absolute -top-5 h-16 object-cover rounded-full" src="https://leatsophat.me/static/images/2022/1.jpg?w=750&q=75" alt="">
+    </div>
+    <div class="flex px-7 flex-col w-full justify-start items-center">
+        <h1 class="w-full font-medium">Adventure / Action</h1>
+        <p class="w-full text-xs">65.7k Follower</p>
+    </div>
+    <button class="bg-[#092857] px-3 py-1 text-white rounded-full text-sm" type="button">Follow</button>
+</div>
